503bbf879845b3d652e7173c118f3084e498ed71
blueshoe <blueshoe>
2016-12-25 20:03:23 +0530

    Initial commit

---
+++ src/PrimeFactor.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PrimeFactor {
+    public static void main(String[] args) {
+        Scanner 
+        System.out.println("Enter a number to generate its prime factors: ");
+
+    }
+}
